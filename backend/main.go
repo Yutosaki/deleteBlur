3e01c2784bbdc39bb1fe3d6819fb2ea19018ad87
Yutosaki <sasayu8190@gmail.com>
2025-04-23 15:57:01 +0900

    feat: add swagger endpoint

--- backend/main.go
+++ backend/main.go
@@ -9,8 +9,14 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Backend prototype is running!")
 }
 
+func serveOpenAPI(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "./openapi.yml")
+}
+
 func main() {
 	http.HandleFunc("/", handler)
+	http.HandleFunc("/docs/openapi.yml", serveOpenAPI)
+
 	fmt.Println("Listening on :8080")
 	http.ListenAndServe(":8080", nil)
 }
