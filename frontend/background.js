47b8130cc81c3feda8bd681fa1436064e60907e0
Yutosaki <sasayu8190@gmail.com>
2025-04-23 14:31:11 +0900

    chore: formatter

--- frontend/background.js
+++ frontend/background.js
@@ -1,12 +1,12 @@
 chrome.action.onClicked.addListener((tab) => {
 	chrome.scripting.executeScript({
-	target: { tabId: tab.id },
-	func: () => {
-		for (const el of document.querySelectorAll("*")) {
-		el.style.filter = "none";
-		el.style.opacity = "1";
-	}
-	},
+		target: { tabId: tab.id },
+		func: () => {
+			for (const el of document.querySelectorAll("*")) {
+			el.style.filter = "none";
+			el.style.opacity = "1";
+		}
+		},
 	});
 });
 
