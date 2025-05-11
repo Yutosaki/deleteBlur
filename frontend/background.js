6004c51c658b3c1c92c3d156abaadc91d9586b57
Yutosaki <sasayu8190@gmail.com>
2025-05-11 13:17:13 +0900

    feat: add api call but it delete images and choices

--- frontend/background.js
+++ frontend/background.js
@@ -2,17 +2,46 @@ chrome.runtime.onInstalled.addListener(() => {
 	chrome.storage.local.set({
 		blurRemovalEnabled: true,
 		adBlockEnabled: true,
+		textReorderEnabled: true,
 	});
 });
 
 chrome.tabs.onUpdated.addListener((tabId, changeInfo) => {
 	if (changeInfo.status === "complete") {
 		chrome.storage.local.get(
-			["blurRemovalEnabled", "adBlockEnabled"],
+			["blurRemovalEnabled", "adBlockEnabled", "textReorderEnabled"],
 			(data) => {
 				if (
 					data.blurRemovalEnabled !== false ||
-					data.adBlockEnabled !== false
+					data.adBlockEnabled !== false ||
+					data.textReorderEnabled !== false
+				) {
+					chrome.scripting.executeScript({
+						target: { tabId: tabId },
+						files: ["content.js"],
+					});
+				}
+			},
+		);
+	}
+});
+chrome.runtime.onInstalled.addListener(() => {
+	chrome.storage.local.set({
+		blurRemovalEnabled: true,
+		adBlockEnabled: true,
+		textReorderEnabled: true,
+	});
+});
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo) => {
+	if (changeInfo.status === "complete") {
+		chrome.storage.local.get(
+			["blurRemovalEnabled", "adBlockEnabled", "textReorderEnabled"],
+			(data) => {
+				if (
+					data.blurRemovalEnabled !== false ||
+					data.adBlockEnabled !== false ||
+					data.textReorderEnabled !== false
 				) {
 					chrome.scripting.executeScript({
 						target: { tabId: tabId },
