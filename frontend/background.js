ee08a583e6649a8e7f0da2aea476c9f14a50ccc9
Yutosaki <sasayu8190@gmail.com>
2025-05-12 17:34:36 +0900

    fix: delete dupulication

--- frontend/background.js
+++ frontend/background.js
@@ -25,30 +25,3 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo) => {
 		);
 	}
 });
-chrome.runtime.onInstalled.addListener(() => {
-	chrome.storage.local.set({
-		blurRemovalEnabled: true,
-		adBlockEnabled: true,
-		textReorderEnabled: true,
-	});
-});
-
-chrome.tabs.onUpdated.addListener((tabId, changeInfo) => {
-	if (changeInfo.status === "complete") {
-		chrome.storage.local.get(
-			["blurRemovalEnabled", "adBlockEnabled", "textReorderEnabled"],
-			(data) => {
-				if (
-					data.blurRemovalEnabled !== false ||
-					data.adBlockEnabled !== false ||
-					data.textReorderEnabled !== false
-				) {
-					chrome.scripting.executeScript({
-						target: { tabId: tabId },
-						files: ["content.js"],
-					});
-				}
-			},
-		);
-	}
-});
